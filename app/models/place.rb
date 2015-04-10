a38684f046645c9a0c381a1e28797fdaf5193f4a
Allen Coin <allencoin@gmail.com>
2015-04-10 04:07:50 +0000

    add additional validations to submission and edit form

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :address, :description, :presence => true
+	validates :name, length: { minimum: 3 }
+	validates :description, length: { minimum: 15, maximum: 1000 }
 end
