baf2042b5a8e52aeb23db5ec6bd521bc588c6a2c
Allen Coin <allencoin@gmail.com>
2015-04-10 04:40:16 +0000

    set up maps integration with Bing maps API key

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	geocoded_by :address
+	after_validation :geocode
+
 	validates :name, :address, :description, :presence => true
 	validates :name, length: { minimum: 3 }
 	validates :description, length: { minimum: 15, maximum: 1000 }
