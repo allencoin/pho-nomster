498bf207495d7a5300bfa54a76eba4ac6f570829
Allen Coin <allencoin@gmail.com>
2015-04-15 09:51:34 +0000

    make adding a comment trigger an automated email to the person who submitted the place

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
+	after_create :send_comment_email
+
+	def send_comment_email
+		NotificationMailer.comment_added(self).deliver
+	end
 end
