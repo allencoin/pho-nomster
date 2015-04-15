efa4cdeaeded8d5492f2dc1bd0af39ef448f9667
Allen Coin <allencoin@gmail.com>
2015-04-15 09:28:12 +0000

    adjust automated mailer to include links to specific place pages in body of email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@wherethepho.com"
 
-  def comment_added
-  	mail(to: "allencoin@gmail.com", subject: "Someone commented on your pho place!")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+
+  	mail(to: @place_owner.email, subject: "Someone commented on #{@place.name}!")
   end
 end
